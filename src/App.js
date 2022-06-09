252b4cb56350b448de04fe9131e3ac405af4583e
Steven Ng <stevenng@Stevens-MacBook-Pro.local>
2022-06-09 17:58:15 -0500

    expense item compoenent and styling added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>THIS IS SOME EXTRA TEXT</p>
 
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
-        <div>
-            <p>Date</p>
-            <h2>Title</h2>
-            <p>Amount</p>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
         </div>
     );
 }
