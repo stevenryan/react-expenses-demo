33d4438ddb8ddbb998173b8ec038d8c9ee192298
Steven Ng <stevenng@Stevens-MacBook-Pro.local>
2022-06-09 16:53:01 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>THIS IS SOME EXTRA TEXT</p>
+
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <p>Date</p>
+            <h2>Title</h2>
+            <p>Amount</p>
+        </div>
+    );
+}
+
+export default ExpenseItem;
